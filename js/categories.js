56963c89537996ccf7a81a429e110b60aa056e15
Lailatul Fitriyah <arwahitem01@gmail.com>
2024-04-03 07:17:17 +0700

    Update categories.js

--- js/categories.js
+++ js/categories.js
@@ -192,10 +192,53 @@ class Categories {
             let filteredProducts = data.filter(product => product.rating.rate <= value);
             // Bersihkan konten produk sebelum menambahkan produk yang difilter
             $(".products").empty();
-            // Loop melalui produk yang difilter dan tambahkan ke halaman
+
+
             filteredProducts.forEach(function(product) {
-                // Tambahkan kode untuk menambahkan produk ke halaman di sini
-                console.log(product);
+                // ====================================
+                // category name
+                $(".category-name").html(toTitleCase(product.category));
+                // ====================================
+                // home link and name of category page
+                $(".breadcrumb").html(
+                    '<a class="link-danger link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="/">Home</a> <span> >></span>' +
+                    toTitleCase(product.category)
+                );
+                // ====================================
+                // products card
+                $(".products").append(
+                    '<div  class="col mx-auto cards products__' +
+                    product.category +
+                    '"><div class="card products__card"><a class="text-decoration-none position-relative h-50 w-100" href="../product.html?productid=' +
+                    encodeURIComponent(product.id) +
+                    '">' +
+                    ' <img class="card-img-top position-absolute top-50 start-50 translate-middle products__img " src = " ' +
+                    product.image +
+                    '" alt =' +
+                    product.title +
+                    ' > </img ></a><div class="d-flex justify-content-center align-items-center" ><a class="text-decoration-none" href="../product.html?productid=' +
+                    encodeURIComponent(product.id) +
+                    '">' +
+                    '<h5 class="card-title text-center fs-5 fw-bold text-dark mt-md-2 mt-0" > ' +
+                    product.title +
+                    "</h5 >" +
+                    "<p class=' card-text text-secondary text-center mb-0 mb-md-2' >" +
+                    product.category +
+                    "</p >" +
+                    "<p class=' card-text text-danger text-center ' > <span>$</span> " +
+                    product.price.toFixed(2) +
+                    '</p></div> </a><div class=" d-flex justify-content-center align-items-center my-0 my-md-3 text-center products__buttons"><button class="btn btn-success mx-2 my-md-1 my-0 product__button cart-button" onclick="addToCart(' +
+                    product.id +
+                    ', this)">' +
+                    " Add To Cart </button>" +
+                    '<button class="btn btn-primary my-md-1 my-0 mx-2 product__button wish-button " onclick="addToWish(' +
+                    product.id +
+                    ',this)">' +
+                    " Add To Wishlist " +
+                    '</button></div><div class="card-footer position-absolute bottom-0  w-100 text-center">' +
+                    '<small class="text-body-secondary">Last updated 3 days ago</small></div></div></div>'
+                );
+
             });
         });
     }
